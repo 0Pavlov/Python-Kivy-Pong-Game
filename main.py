dfdb0acd3d86c5d3dcd170f2cde0e07dd8a3ed1f
0Pavlov <ffsvttvscsvb@gmail.com>
2024-06-05 10:44:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,168 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.app import Builder
+from kivy.properties import (
+    NumericProperty,
+    ReferenceListProperty,
+    ObjectProperty,
+    ListProperty,
+)
+from kivy.vector import Vector
+from kivy.clock import Clock
+
+Builder.load_string(
+    """
+<PongGame>:
+    ball: pong_ball
+    player: player
+    opponent: opponent
+
+    canvas:
+        Color:
+            rgba: 1, 1, 1, 1    
+        Rectangle:
+            id: background
+            size: self.width, self.height / 2
+            pos: 0, self.height / 2
+
+    Label:
+        id: opponent_score
+        font_size: 70
+        color: 0, 0, 0, 1
+        center_x: root.width / 2
+        center_y: root.top - 100
+        text: str(root.player.score)
+
+    Label:
+        id: player_score
+        font_size: 70
+        center_x: root.width / 2
+        center_y: root.y + 100
+        text: str(root.opponent.score)
+
+    PongBall:
+        id: pong_ball
+        center: self.parent.center
+
+    PongPaddle:
+        id: opponent
+        center_x: root.width / 2
+        y: root.y + 100
+        color: 1, 1, 1, 1
+
+    PongPaddle:
+        id: player
+        center_x: root.width / 2
+        y: root.top - self.height - 100
+        color: 0, 0, 0, 1
+
+<PongBall>:
+    size: 70, 70
+    canvas:
+        Color:
+            rgba: root.color
+        Ellipse:
+            pos: self.pos
+            size: self.size
+
+<PongPaddle>:
+    id: pong_paddle
+    score: root.score
+    size: 200, 50
+    on_score: self.size[0] -= 50
+    canvas:
+        Color:
+            rgba: root.color
+        Rectangle:
+            pos: self.pos
+            size: self.size
+"""
+)
+
+
+class PongGame(Widget):
+    player = ObjectProperty(None)
+    opponent = ObjectProperty(None)
+    ball = ObjectProperty(None)
+
+    def change_color(self):
+        if self.ball.center_y > self.center_y:
+            self.ball.color = [0, 0, 0, 1]
+        elif self.ball.center_y < self.center_y:
+            self.ball.color = [1, 1, 1, 1]
+
+    def serve_ball(self, vel=(0, 4)):
+        self.ball.center = self.center
+        self.ball.vel = vel
+
+    def update(self, dt):
+        self.change_color()
+        self.ball.move()
+
+        # Bounce off paddles
+        self.player.bounce_ball(self.ball)
+        self.opponent.bounce_ball(self.ball)
+
+        # Bounce x
+        if self.ball.x < 0 or self.ball.right > self.width:
+            self.ball.vel_x *= -1
+        # Bounce y
+        if self.ball.top > self.height:
+            self.opponent.score += 1
+            self.serve_ball(vel=(0, 4))
+        if self.ball.y < self.y:
+            self.player.score += 1
+            self.serve_ball(vel=(0, -4))
+
+    def on_touch_move(self, touch):
+        if touch.y < self.height / 2:
+            self.opponent.center_x = touch.x
+        if touch.y > self.height - self.height / 2:
+            self.player.center_x = touch.x
+
+
+class PongBall(Widget):
+    color = ListProperty([0.5, 0.5, 0.5, 1])
+
+    # Ball velocity
+    vel_x = NumericProperty(0)
+    vel_y = NumericProperty(0)
+    vel = ReferenceListProperty(vel_x, vel_y)
+
+    def move(self):
+        self.pos = Vector(*self.vel) + self.pos
+
+
+class PongPaddle(Widget):
+    color = ListProperty([1, 1, 1, 1])
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        # 70 is the ball size defined in kv
+        if self.collide_widget(ball):
+            if (self.collide_point(
+                    ball.center_x, ball.center_y - 70
+            ) and ball.vel[1] < 0) or (self.collide_point(ball.center_x, ball.center_y + 70) and ball.vel[1] > 0):
+                vx, vy = ball.vel[0], ball.vel[1]
+                offset = (ball.center_y - self.center_y) / (self.height / 2)
+                bounced = Vector(vx, vy * -1)
+                vel = bounced * 1.1
+                ball.vel = vel[0] + offset, vel[1]
+            elif self.collide_point(
+                    ball.center_x - 70, ball.center_y
+            ) or self.collide_point(ball.center_x + 70, ball.center_y):
+                ball.vel = Vector(ball.vel[0] * -1, ball.vel[1])
+            else:
+                pass
+
+
+class PongApp(App):
+    def build(self):
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0 / 120.0)
+        return game
+
+
+if __name__ == "__main__":
+    PongApp().run()
